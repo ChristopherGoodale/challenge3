7ea96aac8df89b30a380e4c434fdf229a27050cf
ChristopherGoodale <christophergdale@gmail.com>
2022-11-13 17:36:58 -0600

    Update script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,35 @@
 // Assignment code here
+let generateBtn = document.querySelector("#generate");
+
 //This is my database. Numbers, Lowercase letters, Uppercase letters, and Special characters
-let lowerLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-let upperLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-let numberCharacters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-let specialCharacters = ["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", "|", "\\", ":", ";", "'", "<", ",", ">", ".", "?", "/"];
+let characters = {
+  lowerCase =['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+  upperCase =['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+  numbers =[0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+  special =["~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", "|", "\\", ":", ";", "'", "<", ",", ">", ".", "?", "/"],
+};
+let input;
+let choices;
+let hasLowerCase;
+let hasUpperCase;
+let hasNumeric;
+let hasSpecialCharacters;
+
+function generatePassword() { }
+
 let passwordMinLength = 8;
 let passwordMaxLength = 128;
 let randomInt = getRandomNC(numberCharacters.length);
-let password = numberCharacters[randomInt];
+let password = [];
 console.log(lowerLetters)
 console.log(lowerLetters.length)
 
 
-let lengthEntry = window.prompt('Your password must be between 8 and 128 characters. Please enter the desired length:')
-console.log(lengthEntry)
+let lengthEntry = window.prompt('Your password must be between 8 and 128 characters. Please enter the desired length:');
+console.log(lengthEntry);
 if (lengthEntry < passwordMinLength || lengthEntry > passwordMaxLength) {
   let lengthCheckBad = window.alert('Oops, it looks like that is not an acceptable length. Please start over!');
   console.addEventListener(lengthCheckBad, ('click') => {
-    EventTarget.
   })
 }
 
@@ -28,11 +40,12 @@ if (lengthEntry < passwordMinLength || lengthEntry > passwordMaxLength) {
 //}
 
 // Get references to the #generate element
-let generateBtn = document.querySelector("#generate");
+
 
 //This function will generat eht password and retunr it to the correct password
 function generatePassword() {
-  //
+  prompt("How many characters is in your password? (8-128 characters)")
+  )
   return password;//predefined password
 }
 
